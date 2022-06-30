da6ab2943222f53317f7ae179d996cb207f44c1b
Jan Zabłocki <kontakt@janzablocki.pl>
2022-06-30 23:52:23 +0200

    fix: Fix a weird typescript bug that blocked CommonJS module import via require

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const { user, host, database, password, port } = require('../secrets/db_configuration');
 
-const pool = new Pool({ user, host, database, password, port });
+var pool = new Pool({ user, host, database, password, port }); // using var because typescript throws a ridiculous error about block scoping when i try to import this via CommonJS require.
 
 module.exports = pool;
