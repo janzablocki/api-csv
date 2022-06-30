c168375567fdf032027a3f5b3fed1e3841aaaddc
Jan Zabłocki <kontakt@janzablocki.pl>
2022-07-01 00:32:22 +0200

    feat: Add /users endpoint that allows to retrieve users and their team

--- routes/users.ts
+++ routes/users.ts
@@ -1,10 +1,28 @@
 const { Router } = require('express');
 import { Request, Response } from 'express';
+var pool = require('../db/index');
 
 const router = Router();
 
-router.get('/', (_req:Request, res:Response) => {
-  res.send('Hello World!');
+router.get('/', (_request:Request, response:Response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC')
+    .then((result: any) => {
+      response.json(result.rows);
+    },
+    ).catch((err: any) => {
+      response.status(500).send(err);
+    });
+});
+
+router.get('/:id', (request:Request, response:Response) => {
+  const id = parseInt(request.params.id);
+  pool.query('SELECT * FROM users WHERE id = $1', [id])
+    .then((result: any) => {
+      response.json(result.rows);
+    })
+    .catch((err: any) => {
+      response.status(500).send(err);
+    });
 });
 
 module.exports = router;
